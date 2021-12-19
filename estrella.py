82a94496ea11f97054956aaa865c9091e6a35ffb
maariagarrcia <mrrmn@myuax.com>
2021-12-19 22:06:21 +0100

    if minimo  puntas

--- estrella.py
+++ estrella.py
@@ -9,19 +9,28 @@ import turtle
 ####
 # F U N C I O N E S
 ####
-def dibujar_estrella():
+def dibujar_estrella(puntas,x,y):
     # Dibujar una estrella regular dado el numero de puntas
+
+
+    def mcd(x,y):
+        # Funciun para calcular el maximo comun divisor de dos 
+        # numeros enteros por el algoritmo de euclides. 
+        # Funciun necesaria para calcular el angulo de rotacion 
+        while y != 0:
+            x, y = y, x % y
+        return x
+
+    # Miinimo  una estrella tiene que tener 4 puntas
+    if puntas <= 4:
+        print("La estrella tiene que tener 4 puntas minimo. Inserte una mayor")
+        return 
     
 
 
 
 
-def mcd(x,y):
-    # Función para calcular el máximo común divisor de dos 
-    # números enteros por el algoritmo de euclides. 
-    # Función necesaria para calcular el angulo de rotacion 
-    while y != 0:
-            x, y = y, x % y
-    return x
+
+
 
 dibujar_estrella()
