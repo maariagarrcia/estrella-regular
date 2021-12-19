2bd8df8b0d3239b1485c6696ea17095fb0e8617a
maariagarrcia <mrrmn@myuax.com>
2021-12-19 21:24:48 +0100

    esquema programa

---
+++ estrella.py
@@ -0,0 +1,16 @@
+####
+# I M P O R T S
+####
+import turtle
+
+####
+# F U N C I O N E S
+####
+def dibujar_estrella():
+    # Dibujar una estrella regular dado el numero de puntas
+
+
+
+def mcd(x,y):
+
+dibujar_estrella()
