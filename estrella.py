8a85b6604a464d0d33a1f649a24d520ee1e5efa4
maariagarrcia <mrrmn@myuax.com>
2021-12-19 23:03:04 +0100

    bucle y color

--- estrella.py
+++ estrella.py
@@ -2,25 +2,31 @@
 # I M P O R T S
 ####
 
-# Import turtle: se usa para crear dibujos sencillos, en una
-# ventana distinta a la ventana  de IDLE.
-import turtle
+
 
 ####
 # F U N C I O N E S
 ####
-def dibujar_estrella(puntas,x,y):
+def dibujar_estrella(puntas,lado=150):
+    # Import turtle: se usa para crear dibujos sencillos, en una
+    # ventana distinta a la ventana  de IDLE.
+    import turtle 
+    
+    turtle.pencolor("pink")
+    turtle.shape("triangle")
+    turtle.fillcolor("yellow")
+    turtle.color("pink")
+    turtle.begin_fill()
+    
     # Dibujar una estrella regular dado el numero de puntas
-
-
     def mcd(x,y):
         # Funciun para calcular el maximo comun divisor de dos 
         # numeros enteros por el algoritmo de euclides. 
         # Funciun necesaria para calcular el angulo de rotacion 
-        while y != 0:
+        while (y != 0):
             x, y = y, x % y
         return x
-
+   
     # Miinimo  una estrella tiene que tener 4 puntas
     if puntas <= 4:
         print("La estrella tiene que tener 4 puntas minimo. Inserte una mayor")
@@ -31,11 +37,17 @@ def dibujar_estrella(puntas,x,y):
         if mcd(puntas, a) == 1:
             angle = 360.0 / puntas * a
             break
+
     
+    # Dibujar los  traz.
+    for _ in range(puntas):
+        # Dibujamos el lado
+        turtle.forward(lado)
+        # Rotamos el cursor
+        turtle.left(angle)
+    turtle.end_fill()
+    return
+    
+dibujar_estrella(10)
 
 
-
-
-
-
-dibujar_estrella()
