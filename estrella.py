74d2eba7fe38d413963bea790dd22c8dad35d0f2
maariagarrcia <mrrmn@myuax.com>
2021-12-19 21:51:20 +0100

    mcd

--- estrella.py
+++ estrella.py
@@ -1,6 +1,9 @@
 ####
 # I M P O R T S
 ####
+
+# Import turtle: se usa para crear dibujos sencillos, en una
+# ventana distinta a la ventana  de IDLE.
 import turtle
 
 ####
@@ -8,9 +11,17 @@ import turtle
 ####
 def dibujar_estrella():
     # Dibujar una estrella regular dado el numero de puntas
+    
+
 
 
 
 def mcd(x,y):
+    # Función para calcular el máximo común divisor de dos 
+    # números enteros por el algoritmo de euclides. 
+    # Función necesaria para calcular el angulo de rotacion 
+    while y != 0:
+            x, y = y, x % y
+    return x
 
 dibujar_estrella()
