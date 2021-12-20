019b06d41a2381e1c426caa9ca4e3091805ae8bb
maariagarrcia <mrrmn@myuax.com>
2021-12-20 23:24:19 +0100

    arreglado error  de poligono estrellado de 6 vertices y reordenacion

--- estrella.py
+++ estrella.py
@@ -16,39 +16,73 @@
 # Import turtle: se usa para crear dibujos sencillos, en una
 # ventana distinta a la ventana de IDLE.
 import turtle 
-    turtle.pencolor("pink")
-    turtle.shape("triangle")
-    turtle.fillcolor("yellow")
-    turtle.color("pink")
-    turtle.begin_fill()
+   
 
-    # Dibujar una estrella regular dado el numero de puntas
-    def mcd(x,y):
-        # Funcion para calcular el maximo comun divisor de dos 
-        # numeros enteros por el algoritmo de euclides. 
-        # Funciun necesaria para calcular el angulo de rotacion 
-        while (y != 0):
-            x, y = y, x % y
-        return x
+def mcd(x,y):
+    # Funcion para calcular el maximo comun divisor de dos 
+    # numeros enteros por el algoritmo de euclides. 
+    # Funciun necesaria para calcular el angulo de rotacion 
+    while (y != 0):
+        x, y = y, x % y
+    return x
    
     
-    def calcular_angulo(puntas):
-        # Bucle el angulo de rotacion
-        for a in range(puntas// 2, 1, -1):
-            if mcd(puntas, a) == 1:
-                return 360.0 / puntas * a
+def calcular_angulo(puntas):
+    # Bucle el angulo de rotacion
+    for a in range(puntas// 2, 1, -1):
+        if mcd(puntas, a) == 1:
+            return 360.0 / puntas * a
 
-        return  # Esto devuele nulo ya que no se especifica un      valor a devolver
+    return  # Esto devuele nulo ya que no se especifica un      valor a devolver
 
-    def dibujar_poligono_estrellado():
-       
+def dibujar_poligono_estrellado(puntas, lado=150):
+    # Dibujar una poligono estrellado regular (con todos los angulo iguales)
+    # # dado el numero de puntas de dicho polígno
+
+    # Bucle para buscar el angulo de rotacion
+    angle = calcular_angulo(puntas)
+
+    # Mínimo  una estrella tiene que tener 4 puntas
+    if not angle:
+        print("No se puede dibujar un poligono estrellado de " +
+              str(puntas) + " vértices")
+        print("Pruebe con un número de vertices distinto y siempre mayor que 4...")
+        return
+
+    # Especifica el color del trazo del dibujo
+    turtle.pencolor("pink")
+
+    # Cambia la forma del "cursor de dibujo" a un triangulo
+    turtle.shape("triangle")
+
+    # Especifica el color del "cursor de dibujo"
+    turtle.color("pink")
+
+    # Indica que queremos que la figura que vamos a dibujar se rellene en color amarillo
+    turtle.begin_fill()
+
+    # Bucle que dibujar la figura arista a arista
+    for _ in range(puntas):
+        # Dibujamos una de las aristas cuya longitud es "lado" avanzando hacia donde
+        # apunta la tortuga (cursor de dibujo)
+        turtle.forward(lado)
+
+        # Rotamos el cursor de dibujo un angulo que depende del número de puntas
+        # y que hemos calculado previamente.
+        turtle.left(angle)
+
+    # Indicamos que ya hemos finalizado el trazado de la figura que empezamos
+    # con begin_fill() y que ya se puede rellenar
+    turtle.fillcolor("yellow")
+    turtle.end_fill()
+    return   
 
 ####
 # I N I C I O   P R O G R A M A
 ####
 
 # Devolvemos la funcion con el parametro a elegir
-dibujar_poligono_estrellado(5)
+dibujar_poligono_estrellado(6)
 
 
 
